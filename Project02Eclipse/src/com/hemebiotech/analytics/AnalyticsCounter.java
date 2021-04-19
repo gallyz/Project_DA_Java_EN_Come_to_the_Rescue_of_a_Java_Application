c88780605054aa4d3123ef185629001a16a7809a
pierre.garcia <pierre.garcia40@outlook.fr>
2021-04-19 17:42:47 +0200

    Maj des paths dans le main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,8 +6,8 @@ public class AnalyticsCounter {
 	
 	//Paths implementation for the input and output files
 	
-	private static String SYMPTOMS_PATH = "C:\\Users\\Gally\\Eclipse Logiciel\\Workspace\\P2 Hemebiotecth\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt";
-	private static String SYMPTOMS_RESULT ="C:\\Users\\Gally\\Eclipse Logiciel\\Workspace\\P2 Hemebiotecth\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\result.txt";
+	private static String SYMPTOMS_PATH = "symptoms.txt";
+	private static String SYMPTOMS_RESULT ="result.out";
 	
 	public static void main(String args[]) throws Exception {
 		
